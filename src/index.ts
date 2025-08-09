89aaac0ecf173dc45ec8d7611b1fd7f7b92a76ab
Ana Lúcia <ana.medeiros0097@gmail.com>
2025-08-08 22:00:30 -0300

    feat: add prisma integration and connection with database

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
+import { prisma } from "./prisma.js"
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 })
 
+app.get("/products", async (req, res) => {
+  const products = await prisma.product.findMany();
+  res.json(products);
+});
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
